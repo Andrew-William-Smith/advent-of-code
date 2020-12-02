517214abc8c31a6738125fe25651371cf77c0de7
Andrew-William-Smith <aws@awsmith.us>
2020-12-02 14:51:11 -0600

    2020 Day 2 solution refinements
    ~ <= can take an arbitrary number of parameters
    ~ Defined a function for character equality at a 1-based index

--- day2.lisp
+++ day2.lisp
@@ -12,7 +12,7 @@
   "Return a PASSWORD instance for the password defined in the SPEC."
   (cl-ppcre::register-groups-bind
    ((#'parse-integer min max) (#'string-to-char char) password)
-   ("(\\d+)-(\\d+) (.): (.*)" spec)
+   ("(\\d+)-(\\d+) ([a-z]): ([a-z]+)" spec)
    (when password
      (make-password :min-index min
                     :max-index max
@@ -23,21 +23,25 @@
 
 (defun valid-count-occurrences (password)
   "Determine if the specified PASSWORD is valid according to Part 1."
-  (let ((occurrences (count (password-char password) (password-data password))))
-    (and (>= occurrences (password-min-index password))
-         (<= occurrences (password-max-index password)))))
+  (<= (password-min-index password)
+      (count (password-char password) (password-data password))
+      (password-max-index password)))
 
 ;; Part 1: Determine how many passwords have occurrences of the character within
 ;;         the specified limit.
 (print (format t "Part 1: ~d passwords are valid"
                (count-if #'valid-count-occurrences *input*)))
 
+(defun char-at-index= (string index target)
+  "Determine whether the character at the specified 1-based INDEX in the specified STRING is equal to the TARGET character."
+  (char= (char string (1- index)) target))
+
 (defun valid-index-occurrences (password)
   "Determine if the specified PASSWORD is valid according to Part 2."
   (let ((search-char (password-char password))
         (data (password-data password)))
-    (neq (char= (char data (1- (password-min-index password))) search-char)
-         (char= (char data (1- (password-max-index password))) search-char))))
+    (neq (char-at-index= data (password-min-index password) search-char)
+         (char-at-index= data (password-max-index password) search-char))))
 
 ;; Part 2: Determine how many passwords have occurrences of the character only
 ;;         at one of the specified indices.
