2daa13e2866d885aa5b95f2d2218296df5964d49
Andrew-William-Smith <aws@awsmith.us>
2020-12-05 00:54:59 -0600

    2020 Day 5: Added solution
    Today's problem was substantially simpler than yesterday's, if for
    no other reason than that it didn't require too much input parsing.
    Nevertheless, it allowed me to continue developing my skills with
    the LOOP macro: at this point, I think I've used almost every
    directive in an Advent of Code solution at some time or another.
    
    My solution for Part 2 is the simple arithmetic approach.  I know
    that you can use the bit-twiddling expression:
      >> (XOR over list) XOR (XOR over (range list))
    To determine a missing integer in a sequence as well, but that
    honestly wasn't necessary for today's solution since the sum of all
    of the seat ID's wouldn't even overflow a 32-bit integer.  I went
    with the arithmetic solution largely because I liked the code a bit
    better, although I've confirmed that both will work in Lisp.

---
+++ day5.lisp
@@ -0,0 +1,35 @@
+(load "common.lisp")
+
+(defun parse-binary (spec one)
+  "Parse the specified seat assignment SPEC to an integer, with the specified
+   ONE character mapping to a bit value 1 and all others mapping to 0."
+  (parse-integer (map 'string [if (char= % one) #\1 #\0] spec) :radix 2))
+
+(defun parse-seat (spec)
+  "Parse a seat assignment from the specified string SPEC.  Returns the seat as
+   a (ROW . COLUMN) cons cell."
+  (cons
+   (parse-binary (subseq spec 0 7) #\B)
+   (parse-binary (subseq spec 7 10) #\R)))
+
+;; Parse each line of the input to a (ROW . COLUMN) pair.
+(defparameter *input* (map-file #p"input/day5.txt" #'parse-seat))
+
+(destructuring-bind (min-id max-id id-total)
+    (loop for seat in *input*
+          for id = (+ (* 8 (car seat)) (cdr seat))
+          minimize id into min
+          maximize id into max
+          sum id into total
+          finally (return (list min max total)))
+
+  ;; Part 1: Determine the highest seat ID on any boarding pass
+  (format t "Part 1: ~d~%" max-id)
+
+  ;; Part 2: Determine my seat, the only unoccupied seat on the plane knowing
+  ;;         that some seats at the front or back of the plane may be missing.
+  ;;         We use the property that the difference between the sum of all
+  ;;         occupied seat ID's and the sum of the sequence from the minimum to
+  ;;         the maximum seat ID should be the ID of my seat.
+  (format t "Part 2: ~d~%"
+          (- (loop for i from min-id to max-id sum i) id-total)))
